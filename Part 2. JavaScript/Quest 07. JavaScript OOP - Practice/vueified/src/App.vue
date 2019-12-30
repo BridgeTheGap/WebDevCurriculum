8520921e012e66c7893055ecb34a5519b4e6fc7e
Josh Woomin Park <joshuaparkwm@gmail.com>
2019-12-30 09:40:59 +0900

    Update: Q07 - double click handler

--- Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/vueified/src/App.vue
+++ Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/vueified/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="root">
-    <Desktop v-if="root" :content="root.content" @onDoubleClick="onDoubleClick" />
+    <Desktop v-if="root" :content="root.content" />
   </div>
 </template>
 
@@ -8,7 +8,6 @@
 /* eslint-disable no-console */
 import store from './store.js';
 import Desktop from './components/Desktop';
-import { QFile, QDirectory } from './file.js';
 
 export default {
   name: 'app',
@@ -16,25 +15,6 @@ export default {
     return store.state;
   },
   components: { Desktop },
-  methods: {
-    onDoubleClick({ item }) {
-      if (item.file instanceof QDirectory) {
-        // FIXME: QFile parent 구현 후 uncomment
-        // data.file.content = await this.dataManager.loadDirectory()
-        /* mock code */
-        const content = [
-          new QDirectory(`foo_in_${item.file.name}`),
-          new QFile(`bar_in_${item.file.name}`),
-          new QFile(`baz_in_${item.file.name}`)
-        ]; //.map(file => new FileViewData(file));
-        /* mock code end */
-        this.root.setContent(`/${item.file.name}`, content);
-        // this.scene.desktopPresenter.displayDirectory(item.file, content);
-      } else {
-        console.log(`open file ${item.file}`);
-      }
-    }
-  },
   created() {
     store.initializeAction();
   }
--- Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/vueified/src/components/Desktop.vue
+++ Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/vueified/src/components/Desktop.vue
@@ -1,10 +1,43 @@
+<template>
+  <Directory :content="content" @onDoubleClick="onDoubleClick"></Directory>
+</template>
+
 <script>
-/* eslint-disable no-console */
+/* eslint-disable */
 import Directory from './Directory';
+import { QFile, QDirectory } from '../file.js';
 
 export default {
   name: 'Desktop',
-  mixins: [Directory]
+  mixins: [Directory],
+  components: { Directory },
+  data() {
+    return { windowList: null };
+  },
+  methods: {
+    onDoubleClick({ item }) {
+      if (item.file instanceof QDirectory) {
+        // FIXME: QFile parent 구현 후 uncomment
+        // data.file.content = await this.dataManager.loadDirectory()
+        /* mock code */
+
+        const index = this.content.indexOf(item.file);
+
+        const content = [
+          new QDirectory(`foo_in_${item.file.name}`),
+          new QFile(`bar_in_${item.file.name}`),
+          new QFile(`baz_in_${item.file.name}`)
+        ]; //.map(file => new FileViewData(file));
+        /* mock code end */
+
+        this.content[index].setContent(``, content);
+        // this.windowList.push()
+        // this.scene.desktopPresenter.displayDirectory(item.file, content);
+      } else {
+        console.log(`open file ${item.file}`);
+      }
+    }
+  }
 };
 </script>
 
